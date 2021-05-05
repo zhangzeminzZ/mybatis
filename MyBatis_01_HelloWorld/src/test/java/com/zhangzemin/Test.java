6e6d48e2d554bbe5dee302a042875f7e34c52b70
zhangzemin <zhangzeminzz@163.com>
2021-05-05 19:36:04 +0800

    mybatis HelloWorld

---
+++ MyBatis_01_HelloWorld/src/test/java/com/zhangzemin/Test.java
@@ -0,0 +1,16 @@
+package com.zhangzemin;
+
+/*import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;*/
+
+public class Test {
+    /*private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
+    @org.junit.Test
+    public void test01(){
+        LOGGER.error("error");
+        LOGGER.warn("warn");
+        LOGGER.info("info");
+        LOGGER.debug("debug");
+        LOGGER.trace("trace");
+    }*/
+}
